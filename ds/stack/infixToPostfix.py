00777252ac426e2a2a94225550aebd049f2b4fba
Kapil Singh <singh.kapy@gmail.com>
2016-09-29 12:42:59 +0530

    Added infix to postfix

---
+++ ds/stack/infixToPostfix.py
@@ -0,0 +1,47 @@
+import os, sys
+sys.path.append(os.path.join(os.getcwd()))
+from stack import Stack
+
+def infixToPostfix(infixexpr):
+    """
+    Converts given infix expression to postfix expression
+    """
+    ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    DIGITS = "0123456789"
+    
+    postFixList = []
+    
+    prec = {}
+    prec["^"] = 4
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+
+    opStack = Stack()
+    tokenList = infixexpr.split()
+
+    for token in tokenList:
+        if token in ALPHABETS or token in DIGITS:
+            postFixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            while opStack.peek() != '(':
+                postFixList.append(opStack.pop())
+            opStack.pop()
+        else:
+            while not opStack.isEmpty() and (prec[token] <= prec[opStack.peek()]):
+                postFixList.append(opStack.pop())
+            opStack.push(token)
+
+    while not opStack.isEmpty():
+        postFixList.append(opStack.pop())
+
+    return " ".join(postFixList)
+
+
+            
+        
+    
