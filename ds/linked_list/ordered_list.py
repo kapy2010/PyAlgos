e2556795a95e409644ab4a694dabbea6cc7587a2
Kapil Singh <singh.kapy@gmail.com>
2016-10-09 13:07:28 +0530

    Added ordered list

---
+++ ds/linked_list/ordered_list.py
@@ -0,0 +1,69 @@
+import os, sys
+sys.path.append(os.path.join(os.getcwd()))
+from node import Node
+
+class OrderedList:
+
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+        return found
+
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not found:
+            if current.getData() >= item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous == None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+            current.setNext(None)
